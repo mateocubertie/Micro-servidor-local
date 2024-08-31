1dd889cf8bf086e948cd3d3d9db2a31028c00767
Mateo Cubertie <mcubertie@gmail.com>
2024-08-31 01:23:02 -0300

    update_implementado

--- server.js
+++ server.js
@@ -9,38 +9,39 @@ const port = 80;
 let sensorData = {}
 
 const dataPath = path.join(__dirname, 'datos.json')
-const dataJSON = fileSystem.readFileSync(dataPath)
+let dataFile = fileSystem.readFileSync(dataPath)
 
 const indexPath = path.join(__dirname, 'index.html')
-const indexHTML = fileSystem.readFileSync(indexPath)
+let indexHTML = fileSystem.readFileSync(indexPath)
 
-let test = {
-    nombre: 'test'
+
+
+let data = {}
+
+function dataAppend(key, value) {
+    data[`${key}`] = value
+    let dataJSON = JSON.stringify(data)
+    fileSystem.writeFile(dataPath, dataJSON, (err) => {
+        if (err) throw err
+    })
+    dataFile = fileSystem.readFileSync(dataPath)
 }
-let test1 = {
-    nombre: 'test'
-}
-let test2 = {
-    nombre: 'test'
-}
 
-let data = [test, test1, test2]
 
-let testJSON = JSON.stringify(data)
-
-fileSystem.writeFile(dataPath, testJSON, (err) => {
-    if (err) throw err
-})
 
 const server = http.createServer((req, res) => {
     // __dirname -> variable de entorno de node.js con el path al directorio actual
+    let parsedUrl = url.parse(req.url,true)
     if (req.url === '/datos.json') {
         res.setHeader('Content-type', 'application/json')
-        res.write(dataJSON)
+        res.write(dataFile)
     }
-    else if (req.url === '/update') {
-        let parsedQuery = url.parse(req.url, true)
-        
+    else if (parsedUrl.pathname === '/update') {
+        console.log('solicitud de update!!!!')
+        let parsedQuery = url.parse(req.url, true).query
+        for (dato in parsedQuery) {
+            dataAppend(dato, parsedQuery[`${dato}`])
+        }
     }
     else {
         res.setHeader('Content-type', 'text/html; charset: utf-8')
