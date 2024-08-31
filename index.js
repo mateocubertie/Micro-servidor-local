a4742e7006db24e6db54939b8ef3cba5f03f6bdb
Mateo Cubertie <mcubertie@gmail.com>
2024-08-31 01:47:20 -0300

    server_terminado

---
+++ index.js
@@ -0,0 +1,16 @@
+let display = document.querySelector('.showData')
+// IP HARDCODEADA (CAMBIAR!!!!!!!!!!!!!!!!!)
+fetch('http://127.0.0.1/datos.json')
+    .then((res) => { return res.json() })
+    .then((data) => {
+        for (dato in data) {
+            let printData = document.createElement('h2')
+            printData.textContent = `${dato}: ${data[dato]}`
+            display.appendChild(printData)
+        }
+    })
+    .catch(() => {
+        let errorMessage = document.createElement('h2')
+        errorMessage.textContent = 'Falló la conexion con el servidor adquisidor de datos'
+        display.appendChild(errorMessage)
+    })
--- server.js
+++ server.js
@@ -6,41 +6,56 @@ const path = require('path');
 const hostname = '127.0.0.1';
 const port = 80;
 
-let sensorData = {}
-
 const dataPath = path.join(__dirname, 'datos.json')
 let dataFile = fileSystem.readFileSync(dataPath)
 
 const indexPath = path.join(__dirname, 'index.html')
 let indexHTML = fileSystem.readFileSync(indexPath)
 
+const stylePath = path.join(__dirname, 'style.css')
+let styleSheet = fileSystem.readFileSync(stylePath)
+
+const scriptPath = path.join(__dirname, 'index.js')
+let indexScript = fileSystem.readFileSync(scriptPath)
 
 
 let data = {}
 
 function dataAppend(key, value) {
-    data[`${key}`] = value
+    data[key] = value
     let dataJSON = JSON.stringify(data)
     fileSystem.writeFile(dataPath, dataJSON, (err) => {
         if (err) throw err
     })
-    dataFile = fileSystem.readFileSync(dataPath)
+    updateData()
 }
 
+function updateData() {
+    dataFile = fileSystem.readFileSync(dataPath)
+}
 
 
 const server = http.createServer((req, res) => {
     // __dirname -> variable de entorno de node.js con el path al directorio actual
     let parsedUrl = url.parse(req.url,true)
     if (req.url === '/datos.json') {
+        updateData()
         res.setHeader('Content-type', 'application/json')
         res.write(dataFile)
     }
+    else if (req.url === '/style.css') {
+        res.setHeader('Content-type', 'text/css')
+        res.write(styleSheet)
+    }
+    else if (req.url === '/index.js') {
+        res.setHeader('Content-type', 'text/javascript')
+        res.write(indexScript)
+    }
     else if (parsedUrl.pathname === '/update') {
-        console.log('solicitud de update!!!!')
+        console.log('Solicitud de update!!!!')
         let parsedQuery = url.parse(req.url, true).query
         for (dato in parsedQuery) {
-            dataAppend(dato, parsedQuery[`${dato}`])
+            dataAppend(dato, parsedQuery[dato])
         }
     }
     else {
