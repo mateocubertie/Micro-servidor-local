9c9d906a7ae37f28e4ae1e0455893b33dc8c8fa6
Mateo Cubertie <mcubertie@gmail.com>
2024-08-31 01:04:31 -0300

    server_funcionando

--- server.js
+++ server.js
@@ -6,18 +6,47 @@ const path = require('path');
 const hostname = '127.0.0.1';
 const port = 80;
 
+let sensorData = {}
+
+const dataPath = path.join(__dirname, 'datos.json')
+const dataJSON = fileSystem.readFileSync(dataPath)
+
+const indexPath = path.join(__dirname, 'index.html')
+const indexHTML = fileSystem.readFileSync(indexPath)
+
+let test = {
+    nombre: 'test'
+}
+let test1 = {
+    nombre: 'test'
+}
+let test2 = {
+    nombre: 'test'
+}
+
+let data = [test, test1, test2]
+
+let testJSON = JSON.stringify(data)
+
+fileSystem.writeFile(dataPath, testJSON, (err) => {
+    if (err) throw err
+})
+
 const server = http.createServer((req, res) => {
     // __dirname -> variable de entorno de node.js con el path al directorio actual
-    var indexPath = path.join(__dirname, 'index.html');
-    var indexLength = fileSystem.statSync(indexPath);
-    res.writeHead(200, {
-        'Content-Type': 'text/html; charset: utf-8',
-        'Content-length': indexLength.size,
-    });
-    var readStream = fileSystem.createReadStream(indexPath);
-    // Inserta el archivo leido en el objeto Writable (respuesta)
-    readStream.pipe(res);
-
+    if (req.url === '/datos.json') {
+        res.setHeader('Content-type', 'application/json')
+        res.write(dataJSON)
+    }
+    else if (req.url === '/update') {
+        let parsedQuery = url.parse(req.url, true)
+        
+    }
+    else {
+        res.setHeader('Content-type', 'text/html; charset: utf-8')
+        res.write(indexHTML)
+    }
+    res.end()
 });
 
 server.listen(port, hostname, () => {
