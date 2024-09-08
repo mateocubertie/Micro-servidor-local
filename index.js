36a5ac219ac420cdd97b65af19b7d8cb48d13506
Mateo Cubertie <mcubertie@gmail.com>
2024-09-08 19:39:55 -0300

    update_tiemporeal

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 let display = document.querySelector('.showData')
 //! IP HARDCODEADA (CAMBIAR!!!!!!!!!!!!!!!!!)
 //! TIENE QUE SER LA IP PRIVADA DE LA COMPU EN LA QUE CORRE EL SERVER
-fetch('http://192.168.100.15/datos.json')
-    .then((res) => { return res.json() })
-    .then((data) => {
-        for (dato in data) {
-            let printData = document.createElement('h2')
-            printData.textContent = `${dato}: ${data[dato]}`
-            display.appendChild(printData)
-        }
-    })
-    .catch(() => {
-        let errorMessage = document.createElement('h2')
-        errorMessage.textContent = 'Falló la conexion con la base de datos'
-        display.appendChild(errorMessage)
-    })
+
+let delayStart = true
+
+printDatos()
+actualizarDatos()
+
+function printDatos() {
+    fetch('http://192.168.8.202/datos.json')
+        .then((res) => { return res.json() })
+        .then((data) => {
+            display.innerHTML = "<h1>Dashboard</h1>"
+            for (dato in data) {
+                let printData = document.createElement('h2')
+                printData.textContent = `${dato}: ${data[dato]}`
+                display.appendChild(printData)
+            }
+        })
+        .catch(() => {
+            let errorMessage = document.createElement('h2')
+            errorMessage.textContent = 'Falló la conexion con la base de datos'
+            display.appendChild(errorMessage)
+        })
+}
+
+function actualizarDatos() {
+    setTimeout(() => {
+        printDatos()
+        actualizarDatos()
+    }, 1000)
+}
+
