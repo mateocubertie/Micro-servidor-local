536af33bc8a5e7f7e96a9c37ec562d1e6889013a
Mateo Cubertie <mcubertie@gmail.com>
2024-10-27 22:23:06 -0300

    status_response

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ const port = 3000;
 // Tell express to use the body-parser middleware and to not parse extended bodies
 app.use(bodyParser.json({ extended: false }))
 
-// Route that receives a POST request to /sms
+// Route that receives a POST request to /update
 app.post('/update', function (req, res) {
     const body = req.body.Body
     console.log(req.body)
+    res.status(200).send('OK')
 })
 
 // Tell our app to listen on port 3000
